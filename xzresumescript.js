37d720b6c8f0274496a385f84df895d9d37b8921
git <git@vmCentServer.com>
2018-10-23 00:53:16 -0400

    Added CSS style 2
    	* Added CSS style 2, not finished. Need fine tune and RWD
    	* Styles can be switched using CSS style buttons

--- xzresumescript.js
+++ xzresumescript.js
@@ -8,6 +8,8 @@ var navDropDownContent = document.getElementsByClassName("nav-dropdown-content")
 var navMenuButton = document.getElementsByClassName("menu-button");
 var navHorizontalBar = document.getElementsByClassName("nav-horizontal-bar");
 var contactButton = document.getElementsByClassName("contact-button");
+var stylesButtons = document.getElementsByClassName("styles-button");
+var headElements = document.getElementsByTagName("head");
 
 for (var i = 0; i < 4; i++) {
 	lwButtons[i].addEventListener('click', showModalButtonClicked);
@@ -56,6 +58,29 @@ function navMenuButtonClicked() {
 	}
 }
 
+for (var i = 0; i < 3; i++) {
+	stylesButtons[i].addEventListener('click', swapCSSButtonClicked);
+}
+
+function swapCSSButtonClicked() {
+	var currentCssLinks = document.getElementsByTagName("link");
+
+	var cssFileNumber = Number(this.value) + 1;
+	var newlink1FileName = "xzoverallstyle" + cssFileNumber + ".css";
+	var newLink1 = document.createElement("link");
+	newLink1.setAttribute("rel", "stylesheet");
+	newLink1.setAttribute("type", "text/css");
+	newLink1.setAttribute("href", newlink1FileName);
+	var newlink2FileName = "xzstyle" + cssFileNumber + ".css";
+	var newLink2 = document.createElement("link");
+	newLink2.setAttribute("rel", "stylesheet");
+	newLink2.setAttribute("type", "text/css");
+	newLink2.setAttribute("href", newlink2FileName);
+
+	headElements[0].replaceChild(newLink1, currentCssLinks[0]);
+	headElements[0].replaceChild(newLink2, currentCssLinks[1]);
+}
+
 var floatingContactButton = document.getElementsByClassName("floating-contact-button");
 var stylesDiv = document.getElementsByClassName("styles");
 go();
@@ -89,17 +114,29 @@ function go() {
 	}
 }
 
+var currentCssLinks = document.getElementsByTagName("link");
 window.onscroll = function() { windowOnScroll()};
 
 function windowOnScroll() {
 	navDropDownContent[0].style.display = "none";
 	navMenuButton[0].style.backgroundColor = "rgb(67, 67, 67)";
-	if (window.pageYOffset > 120) {
-		navHorizontalBar[0].style.boxShadow = "0 3px 6px 0px rgba(0, 0, 0, 0.3)";
+	var cssFile1Url = currentCssLinks[0].href;
+	var currentStyleArr = cssFile1Url.match(/xzoverallstyle[0-9]\.css/g);
+	if (currentStyleArr) {
+		var currentStyleStr = currentStyleArr[currentStyleArr.length - 1];
+		var currentStyleNumberStr = currentStyleStr.replace(/[^0-9]/g, '');
+		if (currentStyleNumberStr == "1") {
+			if (window.pageYOffset > 120) {
+				navHorizontalBar[0].style.boxShadow = "0 3px 6px 0px rgba(0, 0, 0, 0.3)";
+			}
+			else {
+				navHorizontalBar[0].style.boxShadow = "none";
+			}
+		}
+		else if (currentStyleNumberStr == "2") {
+			navHorizontalBar[0].style.boxShadow = "0 4px 10px 0 rgba(0, 0, 0, 0.18), 0 4px 20px 0 rgba(0, 0, 0, 0.15)";
+		}
 	}
-	else {
-		navHorizontalBar[0].style.boxShadow = "none";
 
-	}
 	go();
 }
